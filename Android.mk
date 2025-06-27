d8c1dcc1f086d9ebaaa908866adc29fada723234
Michael Bestas <mkbestas@lineageos.org>
2025-06-27 03:45:06 +0300

    tegu: Import from BP2A.250605.031.A2
    Change-Id: If0864829b1b2184271b9cefde09f7048f4a99062

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),tegu)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,eb4d461159e16d0ff9f6fadfedeac672ffe722cd)
+$(call add-radio-file-sha1-checked,radio/bl1.img,ee821a428b6b7a65c9f4f116fbe957d0f68efb2f)
+$(call add-radio-file-sha1-checked,radio/bl2.img,e4e15367b264ed9083faa11f713792cb3447f10c)
+$(call add-radio-file-sha1-checked,radio/bl31.img,6c20635b904e8ee1c2582dc5c333e6e0df8f6193)
+$(call add-radio-file-sha1-checked,radio/gcf.img,b09d16214eab67cf1da3be309dba7b854d3e8da4)
+$(call add-radio-file-sha1-checked,radio/gsa.img,3ce33cbeea209b56f1e77364881efc411e7400a2)
+$(call add-radio-file-sha1-checked,radio/gsa_bl1.img,b90ac8310cf398feb72afe297ee31b768974162b)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,b61e605c90da8f6f11d680c11f694d349a790df9)
+$(call add-radio-file-sha1-checked,radio/modem.img,944e4c6a6e48cf4ddd57ac68e36a472861b60982)
+$(call add-radio-file-sha1-checked,radio/pbl.img,364335a39cb6f6cef733fe96fb152998b6cd2d46)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,d1cc776bc71c899bb99e64d621e4f6dff1c4aa05)
+
+endif
---
+++ proprietary/product/etc/CarrierSettings/tmobileprivate_us.pb
@@ -0,0 +1,5 @@
+
+tmobileprivate_usƒøÌâ„*(
+T-Mobile USfast.t-mobile.comp€ "M+
+#carrier_nr_availabilities_int_array:
+cellular_usage_setting_intBâíÖ½
