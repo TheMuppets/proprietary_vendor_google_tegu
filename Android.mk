4e25b9433a995643eb3db99f4cb6488e3e911e82
Michael Bestas <mkbestas@lineageos.org>
2025-11-22 13:44:41 +0200

    tegu: Update to BP3A.251105.015
    Change-Id: Ib227ea204e1ba40827a10008d1205d4f17c49825

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@ $(call add-radio-file-sha1-checked,radio/gcf.img,bb4f9a22344ad2ec2349fe19b94d519
 $(call add-radio-file-sha1-checked,radio/gsa.img,005707e71683eaa6599c34f2b280cc897ce99b6c)
 $(call add-radio-file-sha1-checked,radio/gsa_bl1.img,5a1da79acdc016f3ccd05d585304ab54d909ffaa)
 $(call add-radio-file-sha1-checked,radio/ldfw.img,8d8cf54c9f9b594ec1cfacadb4d16a496bd0be4c)
-$(call add-radio-file-sha1-checked,radio/modem.img,dca9b9d1b41fe4cb150169a7a6003964bcd7f0ad)
+$(call add-radio-file-sha1-checked,radio/modem.img,b81150cbe5e2276fb499efa42f7f64f855233795)
 $(call add-radio-file-sha1-checked,radio/pbl.img,9fb1e8a454f3a90477fb3a02b430faff1eb47013)
 $(call add-radio-file-sha1-checked,radio/tzsw.img,94940e067d49c7774f94cfd7607873d3b60b3ec0)
 
